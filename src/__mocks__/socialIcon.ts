9589d69a6a4d45ae029ca0cc473925a433df86af
Ifrat Binte <ifratbinte@gmail.com>
2022-12-01 10:50:44 +0600

    feat(socialIcon *) : add socialIicon componewt

---
+++ src/__mocks__/socialIcon.ts
@@ -0,0 +1,23 @@
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { IconType } from "react-icons/lib";
+
+export interface SocialIconInterface {
+  id: number;
+  Icon: IconType;
+}
+
+const socialIcon: SocialIconInterface[] = [
+  {
+    id: 1,
+    Icon: FaFacebookF,
+  },
+  {
+    id: 2,
+    Icon: FaLinkedinIn,
+  },
+  {
+    id: 2,
+    Icon: FaTwitter,
+  },
+];
+export default socialIcon;
