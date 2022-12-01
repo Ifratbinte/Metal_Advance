4045699d520f32762585d23a588bb61c0fe59bce
Ifrat Binte <ifratbinte@gmail.com>
2022-12-01 14:14:56 +0600

    base structure init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Footer from "#components/layout/Footer/Footer";
+import HeaderMain from "#components/layout/Header/HeaderMain";
+import "./assets/style/component.scss";
+
+function App() {
+  return (
+    <>
+      <HeaderMain />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
