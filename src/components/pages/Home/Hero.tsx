481681030b29e06a9f879b741f908d01f2dfbd03
Ifrat Binte <ifratbinte@gmail.com>
2022-12-08 12:26:12 +0600

    feat(hero) : slilk slider initialize on hero section

--- src/components/pages/Home/Hero.tsx
+++ src/components/pages/Home/Hero.tsx
@@ -1,36 +1,50 @@
 // import heroContents from "#mocks/heroContent";
 import heroContent from "#mocks/jsonData/home.json";
+import Slider from "react-slick";
 
 const Hero = () => {
   const sliders = heroContent.home.hero.slider;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+  };
   return (
-    <>
-      <section id="hero" className="hero base-bg-light">
-        <div className="container position-relative">
-          {sliders &&
-            sliders.map((slider, i) => {
-              return (
-                <div key={i} className="row gy-5 banner my-auto flex-row-reverse align-items-center justify-content-between">
-                  <div className="col-lg-5">
-                    <div className="banner-img">
-                      <img src={slider.img} alt="" />
+    <section id="hero" className="hero base-bg-light">
+      <Slider {...settings}>
+        {sliders &&
+          sliders.map((slider, i) => {
+            return (
+              <div key={i}>
+                {/* <div className="hero-bg" style={{ background: `url(${slider.img})`, backgroundRepeat: "no-repeat", backgroundPosition: right }}> */}
+                {/* <div className="hero-bg"> */}
+                  <div className="container ">
+                    <div className="row gy-5 banner flex-row-reverse align-items-center justify-content-between hero-inner-content">
+                      <div className="col-lg-5">
+                        <div className="banner-img">
+                          <img src={slider.img} alt="" />
+                        </div>
+                      </div>
+                      <div className="col-lg-7">
+                        <div className="banner-block pe-xl-5">
+                          <h1 className="banner-title fs-64 font-Poppins fw-semibold">{slider.title}</h1>
+                          <p className="banner-desc font-Poppins fs-16 fw-normal">{slider.desc}</p>
+                          <a href="#" className="btn button-base-primary">
+                            Read More
+                          </a>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                  <div className="col-lg-7">
-                    <div className="banner-block pe-xl-5">
-                      <h1 className="title banner-title fs-64 font-Poppins fw-semibold">{slider.title}</h1>
-                      <p className="banner-desc font-Poppins fs-16 fw-normal">{slider.desc}</p>
-                      <a href="#" className="btn button-base-primary">
-                        Read More
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-      </section>
-    </>
+                {/* </div> */}
+              </div>
+            );
+          })}
+      </Slider>
+    </section>
   );
 };
 
