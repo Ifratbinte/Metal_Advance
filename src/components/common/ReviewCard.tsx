0dac5e123c04515418151a76fa8cbe57665c237e
Ifrat Binte Kazi <ifratbinte@gmail.com>
2022-12-08 18:11:28 +0600

    conflict resolve

--- src/components/common/ReviewCard.tsx
+++ src/components/common/ReviewCard.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import { FaQuoteLeft } from "react-icons/fa";
-=======
->>>>>>> 2b0cfb7e720e45628a768eb2f211090fbd06bba7
 
 interface Props {
   thumb: string;
@@ -14,7 +11,6 @@ interface Props {
 
 const ReviewCard: React.FC<Props> = ({ thumb, alt, name, designation, review }) => {
   return (
-<<<<<<< HEAD
     <div className="testimonial-wrap base-bg-white py-3 px-4 h-100">
       <div className="testimonial-item">
         <div className="testimonial-top d-flex align-items-center justify-content-between pb-4">
@@ -29,7 +25,6 @@ const ReviewCard: React.FC<Props> = ({ thumb, alt, name, designation, review }) 
         </div>
         <div className="testimonial-bottom">
           <span className="review fs-16">{review}</span>
-=======
     <div className="testimonial-wrap base-bg-white py-3 px-4">
       <div className="testimonial-item">
         <div className="testimonial-top d-flex align-items-center pb-3">
@@ -41,7 +36,6 @@ const ReviewCard: React.FC<Props> = ({ thumb, alt, name, designation, review }) 
         </div>
         <div className="testimonial-bottom">
           <p>{review}</p>
->>>>>>> 2b0cfb7e720e45628a768eb2f211090fbd06bba7
         </div>
       </div>
     </div>
