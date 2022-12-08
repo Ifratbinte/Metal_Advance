3444b588bd43291ea6449fdf2f0921fd40056bc0
Ifrat Binte <ifratbinte@gmail.com>
2022-12-08 14:07:16 +0600

    feat(testimonial*) : slick slider initialize on testimonial portion

--- src/components/pages/Home/Testimonial.tsx
+++ src/components/pages/Home/Testimonial.tsx
@@ -1,33 +1,41 @@
 import ReviewCard from "#components/common/ReviewCard";
 import SectionHeading from "#components/common/SectionHeading";
 import reviewData from "#mocks/jsonData/review.json";
+import ReviewSlider from "react-slick";
+var Slider = {
+  // autoplay: true,
+  dots: true,
+  infinite: true,
+  // speed: 400,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+};
 
 const Review = () => {
   const reviews = reviewData.review;
   return (
-    <>
       <section className="testimonial section-gap base-bg-light" id="testimonial">
         <div className="container">
           <SectionHeading subtitle={reviews.subtitle} title={reviews.title} />
           <div className="row section-card">
-            <div className="testimonial-slider gap-4 d-flex align-items-center justify-content-between">
-              {reviews.card &&
-                reviews.card.map((review: any, i: any) => {
-                  return (
-                    <ReviewCard
-                      thumb={review.client_thumb}
-                      alt={review.client_alt}
-                      name={review.client_name}
-                      designation={review.client_designation}
-                      review={review.client_review}
-                    />
-                  );
-                })}
-            </div>
+              <ReviewSlider {...Slider}>
+                {reviews.card &&
+                  reviews.card.map((review: any, i: any) => {
+                    return (
+                      <ReviewCard
+                        key={i}
+                        thumb={review.client_thumb}
+                        alt={review.client_alt}
+                        name={review.client_name}
+                        designation={review.client_designation}
+                        review={review.client_review}
+                      />
+                    );
+                  })}
+              </ReviewSlider>
           </div>
         </div>
       </section>
-    </>
   );
 };
 
