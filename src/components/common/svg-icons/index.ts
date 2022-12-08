2a55167fa3ddb04bf9a2de6370c3f78f608fd032
Ifrat Binte <ifratbinte@gmail.com>
2022-12-08 10:00:28 +0600

    feat: svg icon use as icon

---
+++ src/components/common/svg-icons/index.ts
@@ -0,0 +1,10 @@
+import PersonIcon from "./PersonIcon";
+import SupportIcon from "./SupportIcon";
+import UpdateIcon from "./UpdateIcon";
+
+const SvgIcon = {
+  Support: SupportIcon,
+  Update: UpdateIcon,
+  Person: PersonIcon,
+};
+export default SvgIcon;
