cc2caffd5df021aa7d57bd85f568a67085fba56c
Ifrat Binte <ifratbinte@gmail.com>
2022-12-11 11:21:02 +0600

    refactor : slider dot style fix

--- src/components/pages/Home/Testimonial.tsx
+++ src/components/pages/Home/Testimonial.tsx
@@ -4,7 +4,7 @@ import reviewData from "#mocks/jsonData/review.json";
 import ReviewSlider from "react-slick";
 var Slider = {
   autoplay: true,
-  dots: true,
+  // dots: true,
   infinite: true,
   arrows: true,
   slidesToShow: 3,
@@ -14,28 +14,31 @@ var Slider = {
 const Review = () => {
   const reviews = reviewData.review;
   return (
+    <>
+     
       <section className="testimonial section-gap base-bg-light" id="testimonial">
         <div className="container">
           <SectionHeading subtitle={reviews.subtitle} title={reviews.title} />
           <div className="row section-card">
-              <ReviewSlider {...Slider}>
-                {reviews.card &&
-                  reviews.card.map((review: any, i: any) => {
-                    return (
-                      <ReviewCard
-                        key={i}
-                        thumb={review.client_thumb}
-                        alt={review.client_alt}
-                        name={review.client_name}
-                        designation={review.client_designation}
-                        review={review.client_review}
-                      />
-                    );
-                  })}
-              </ReviewSlider>
+            <ReviewSlider {...Slider}>
+              {reviews.card &&
+                reviews.card.map((review: any, i: any) => {
+                  return (
+                    <ReviewCard
+                      key={i}
+                      thumb={review.client_thumb}
+                      alt={review.client_alt}
+                      name={review.client_name}
+                      designation={review.client_designation}
+                      review={review.client_review}
+                    />
+                  );
+                })}
+            </ReviewSlider>
           </div>
         </div>
       </section>
+    </>
   );
 };
 
