b75431483537b827dbedbb47c2e0be2470ff96bf
Ifrat Binte <ifratbinte@gmail.com>
2022-12-12 15:26:51 +0600

    img-fix

---
+++ src/components/MasterLayout.tsx
@@ -0,0 +1,11 @@
+import Footer from "./layout/Footer/Footer";
+import Header from "./layout/Header/HeaderMain";
+function MasterLayout() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+export default MasterLayout;
