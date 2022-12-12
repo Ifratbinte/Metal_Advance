6794fa784348536af47677f7f3198135900b07fb
Ifrat Binte <ifratbinte@gmail.com>
2022-12-12 15:17:44 +0600

    fix : modified menu

--- src/components/pages/Home/Work.tsx
+++ src/components/pages/Home/Work.tsx
@@ -9,7 +9,7 @@ const Work = () => {
       <section id="work" className="work section-gap base-bg-white">
         <div className="container">
           <SectionHeading title={works.title} short_title={works.short_title} />
-          <div className="work-shape-wrap">
+          <div className="work-shape-wrap pt-5">
             <div className="work-item gap-4 d-flex align-items-center justify-content-center">
               {works.item &&
                 works.item.map((work: any, i: any) => {
