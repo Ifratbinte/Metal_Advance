e0d10dacbbd6099042eebb390bdcd2389cd4a7f4
Ifrat Binte <ifratbinte@gmail.com>
2022-12-14 10:20:55 +0600

    feat : add new nav Items on navItems Object

--- src/components/layout/Navigation/NavItems.ts
+++ src/components/layout/Navigation/NavItems.ts
@@ -1,37 +1,126 @@
 const NavItems = [
   {
+    id: 1,
     title: "Home",
     url: "/",
   },
   {
+    id: 2,
     title: "About",
-    url: "/about",
+    url: "/#",
     submenu: [
       {
+        id: 1,
+        title: "Corporate Profile",
+        url: "/corporate-profile",
+      },
+      {
+        id: 2,
         title: "Profile",
         url: "/about-benemoy",
       },
       {
+        id: 3,
         title: "Board of Director",
         url: "/board-of-director",
       },
       {
+        id: 4,
         title: "Senior Management",
         url: "/senior-management",
+      },
+      {
+        id: 5,
+        title: "Group Concern",
+        url: "/group-concern",
       },
     ],
   },
   {
+    id: 3,
     title: "Service",
-    url: "/service",
+    url: "/#",
+    submenu: [
+      {
+        id: 1,
+        title: "Services",
+        url: "/service",
+      },
+      {
+        id: 2,
+        title: "Digital Services",
+        url: "/digital-services",
+      },
+      {
+        id: 3,
+        title: "Mobile Apps",
+        url: "/mobile-apps",
+      },
+      {
+        id: 4,
+        title: "IPO Service",
+        url: "/ipo-service",
+      },
+      {
+        id: 5,
+        title: "Downloads",
+        url: "/download",
+      },
+    ],
   },
   {
-    title: "Download",
-    url: "/download",
+    id: 4,
+    title: "News & Updates",
+    url: "/news-updates",
+    submenu: [
+      {
+        id: 1,
+        title: "Notify for Investors",
+        url: "/notify",
+      },
+      {
+        id: 2,
+        title: "Daily News",
+        url: "/daily-news",
+      },
+      {
+        id: 3,
+        title: "DSE News",
+        url: "/dse-news",
+      },
+      {
+        id: 4,
+        title: "BSEC News",
+        url: "/bsec-news",
+      },
+      {
+        id: 5,
+        title: "FAQs",
+        url: "/faq",
+      },
+    ],
   },
   {
-    title: "Account Open",
-    url: "/acc-open",
+    id: 5,
+    title: "R & D",
+    url: "/#",
+    submenu: [
+      {
+        id: 1,
+        title: "Daily Market Update",
+        url: "/daily-market",
+      },
+      {
+        id: 1,
+        title: "IPO Notes",
+        url: "/ipo-notes",
+      },
+      {
+        id: 1,
+        title: "Technical Chart",
+        url: "/technical-chart",
+      },
+    ],
   },
 ];
 export default NavItems;
