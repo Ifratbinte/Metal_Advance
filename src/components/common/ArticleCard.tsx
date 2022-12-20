e3be2b4b1b3617c6b9b469aee3671585fd31b356
Ifrat Binte <ifratbinte@gmail.com>
2022-12-20 10:37:48 +0600

    feat : use image placeholder

--- src/components/common/ArticleCard.tsx
+++ src/components/common/ArticleCard.tsx
@@ -12,32 +12,8 @@ interface Props {
 
 const ArticleCard: React.FC<Props> = ({ image, alt, date, title, description, link }) => {
   return (
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // <div className="card rounded-0 article-card h-100" data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
     <div className="card rounded-0 article-card h-100" data-aos="fade-up" data-aos-offset="300" data-aos-easing="ease-in-sine">
-      <img src={image} alt={alt} className="card-img-top position-relative" />
+      <img src="https://placekitten.com/640/360" alt={alt} className="card-img-top position-relative" />
       <div className="card-body">
         <span className="card-date base position-absolute">{date}</span>
         <h5 className="card-title font-Inter fw-bold fs-22">{title}</h5>
