157cb087e36f845f326cf6fa381633e12312361b
Ifrat Binte <ifratbinte@gmail.com>
2022-12-21 11:25:50 +0600

    refactor : get token from token folder

--- src/store/api/testimonial.ts
+++ src/store/api/testimonial.ts
@@ -1,6 +1,6 @@
+import TOKEN from "#store/token";
 import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
-const TOKEN = `3124d34ba6d202a6db449b9e7827e19ab1fd4a4f909b2529dedfb640432aea11ceebc032fdd772564d4caa66906983fede41e280d52b6276bf749727163ad654579276856e495df5001ff373f775e752baea23733ae5758d6f8c1a0c5d386a515ab732d82e02ff0c29efa5fbbc07de6cdd9088a282bae6aab2e1e9b2d06aed43`;
 export const testimonial = createApi({
   reducerPath: "testimonial",
   baseQuery: fetchBaseQuery({
