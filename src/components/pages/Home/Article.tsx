816f95b9f31263e4d1055d7ed54fc97fc18a1bdf
Ifrat Binte <ifratbinte@gmail.com>
2022-12-26 10:39:09 +0600

    article date formate change

--- src/components/pages/Home/Article.tsx
+++ src/components/pages/Home/Article.tsx
@@ -12,28 +12,30 @@ const Article = () => {
 
   return (
     <>
-      <section id="article" className="article section-gap base-bg-light">
-        <div className="container">
-          <SectionHeading title={articleTitle.title} short_title={articleTitle.short_title} />
-          <div className="row gy-3 section-card">
-            {articles &&
-              articles.data.map((article: any, i: any) => {
-                return (
-                  <div className="col-lg-4" key={i}>
-                    <ArticleCard
-                      image={CONFIGS.CMS_URL + article.attributes.image.data.attributes.formats.thumbnail.url}
-                      alt={article.attributes.title}
-                      date={article.attributes.createdAt}
-                      title={article.attributes.title}
-                      description={article.attributes.short_description}
-                      link={article.attributes.btn_text}
-                    />
-                  </div>
-                );
-              })}
+      {articles?.data?.length ? (
+        <section id="article" className="article section-gap base-bg-white">
+          <div className="container">
+            <SectionHeading title={articleTitle.title} short_title={articleTitle.short_title} />
+            <div className="row gy-3 section-card">
+              {articles &&
+                articles.data.map((article: any, i: any) => {
+                  return (
+                    <div className="col-lg-4" key={i}>
+                      <ArticleCard
+                        image={CONFIGS.CMS_URL + article.attributes.image.data.attributes.formats.thumbnail.url}
+                        alt={article.attributes.title}
+                        date={article.attributes.createdAt.split("T")[0]}
+                        title={article.attributes.title}
+                        description={article.attributes.short_description}
+                        link={article.attributes.btn_text}
+                      />
+                    </div>
+                  );
+                })}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      ) : null}
     </>
   );
 };
