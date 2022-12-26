eb74594e45a477bc723bc81e361ba81749864ce7
Ifrat Binte <ifratbinte@gmail.com>
2022-12-26 10:30:33 +0600

    refactor : hero saction modified with banner gfx

--- src/components/pages/Home/Hero.tsx
+++ src/components/pages/Home/Hero.tsx
@@ -1,9 +1,9 @@
 import heroContent from "#mocks/jsonData/home.json";
 import { Link } from "react-router-dom";
-import Slider from "react-slick";
 
 const Hero = () => {
-  const sliders = heroContent.home.hero.slider;
+  // const hero = heroContent.home.hero.slider;
+  const hero = heroContent.home.hero;
   const settings = {
     dots: true,
     infinite: true,
@@ -13,8 +13,38 @@ const Hero = () => {
     // autoplay: true,
   };
   return (
-    <section id="hero" className="hero base-bg-light">
-      <Slider {...settings}>
+    <section id="hero" className="hero base-bg-white">
+      <div className="container ">
+        <div className="row gy-5 banner flex-row-reverse align-items-center justify-content-between hero-inner-content">
+          <div className="col-lg-6">
+            {/* <div className="banner-gfx" data-aos="fade-up" data-aos-duration="800"> */}
+            <video className="banner-gfx hero-video" autoPlay muted loop data-aos="fade-up" data-aos-duration="800">
+              <source src="/video/cryptocurrency_3.mp4" />
+            </video>
+            {/* </div> */}
+          </div>
+          <div className="col-lg-5">
+            <div className="banner-block" data-aos="fade-down" data-aos-duration="800">
+              <span className="badge rounded-pill primary-bg">Invest</span>
+              <h1 className="banner-title fs-45 font-Poppins fw-semibold">{hero.title}</h1>
+              <p className="banner-desc font-Poppins fs-16 fw-normal">{hero.desc}</p>
+              <ul className="d-flex list">
+                <li>
+                  <Link to="#" className="btn button-base-primary font-Poppins">
+                    Invest for Future
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#" className="btn button-base-warning-outline font-Poppins">
+                    Get In Touch
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <Slider {...settings}>
         {sliders &&
           sliders.map((slider, i) => {
             return (
@@ -37,11 +67,10 @@ const Hero = () => {
                     </div>
                   </div>
                 </div>
-                {/* </div> */}
               </div>
             );
           })}
-      </Slider>
+      </Slider> */}
     </section>
   );
 };
