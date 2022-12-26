dc90604087ff656a37799e63fb489a5a1024803d
Ifrat Binte <ifratbinte@gmail.com>
2022-12-26 10:36:50 +0600

    styles : styles change

--- src/components/common/ArticleCard.tsx
+++ src/components/common/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaLongArrowAltRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 interface Props {
@@ -15,11 +16,14 @@ const ArticleCard: React.FC<Props> = ({ image, alt, date, title, description, li
     <div className="card rounded-0 article-card h-100" data-aos="fade-up" data-aos-offset="300" data-aos-easing="ease-in-sine">
       <img src={image} alt={alt} className="card-img-top position-relative" />
       <div className="card-body">
-        <span className="card-date base position-absolute">{date}</span>
-        <h5 className="card-title font-Inter fw-bold fs-22">{title}</h5>
+        <span className="card-date date-base danger-bg position-absolute">{date}</span>
+        <h5 className="card-title font-Inter primary fw-bold fs-22">{title}</h5>
         <p className="card-text fs-18 fw-normal">{description}</p>
-        <Link to="#" className="card-link link fw-semibold fs-20">
+        <Link to="#" className="card-link primary fw-semibold fs-20">
           {link}
+          <span className="long-arrow">
+            <FaLongArrowAltRight />
+          </span>
         </Link>
       </div>
     </div>
--- src/components/common/ReviewCard.tsx
+++ src/components/common/ReviewCard.tsx
@@ -11,14 +11,14 @@ interface Props {
 
 const ReviewCard: React.FC<Props> = ({ thumb, alt, name, designation, review }) => {
   return (
-    <div className="testimonial-wrap slider-gap base-bg-light h-100" data-aos="zoom-in-right" data-aos-duration="1000">
+    <div className="testimonial-wrap slider-gap bg-light h-100" data-aos="zoom-in-right" data-aos-duration="1000">
       <div className="testimonial-item">
-        <div className="testimonial-top d-flex align-items-center justify-content-between pb-4">
+        <div className="testimonial-top d-flex align-items-center justify-content-between">
           <div className="author d-sm-flex align-items-sm-center">
             <img src={thumb} alt={alt} className="testimonial-thumb border rounded-circle flex-shrink-0" />
             <div className="testimonial-info">
               <h4 className="testimonial-title font-Poppins fw-medium">{name}</h4>
-              <h5 className="testimonial-desc font-Poppins fw-medium fs-14 danger">{designation}</h5>
+              <h5 className="testimonial-desc font-Poppins fw-medium fs-16 primary">{designation}</h5>
             </div>
           </div>
           <FaQuoteLeft className="quote quote-icon" />
