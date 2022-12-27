3fd3bc59159aed6c7fb627442cddcd5418faf9ac
Ifrat Binte <ifratbinte@gmail.com>
2022-12-27 16:22:39 +0600

    url add for social icon

--- src/__mocks__/socialIcon.ts
+++ src/__mocks__/socialIcon.ts
@@ -1,23 +1,32 @@
-import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { FaFacebookF, FaInstagramSquare, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { IconType } from "react-icons/lib";
 
 export interface SocialIconInterface {
   id: number;
   Icon: IconType;
+  url: string;
 }
 
 const socialIcon: SocialIconInterface[] = [
   {
     id: 1,
     Icon: FaFacebookF,
+    url: "https://www.facebook.com/BenemoySecuritiesLimited",
   },
   {
     id: 2,
     Icon: FaLinkedinIn,
+    url: "linkedin.com/company/benemoy-securities-limited",
   },
   {
     id: 3,
     Icon: FaTwitter,
+    url: "https://twitter.com/Benemoy_",
+  },
+  {
+    id: 3,
+    Icon: FaInstagramSquare,
+    url: "https://www.instagram.com/benemoysecurities/",
   },
 ];
 export default socialIcon;
