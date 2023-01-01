27f89847b10f887ffbe34c854a30b047d4f762eb
Ifrat Binte <ifratbinte@gmail.com>
2023-01-01 13:20:52 +0600

    refactor : senior management page

--- src/components/pages/Management.tsx
+++ src/components/pages/Management.tsx
@@ -2,11 +2,135 @@ import EmployeeCard from "#components/common/EmployeeCard";
 import CONFIGS from "#configs/index";
 import employeeData from "#mocks/jsonData/member.json";
 import { useGetManagementQuery } from "#store/api/management";
+import { useMemo } from "react";
 
 const Management = () => {
   const { data: teams, isLoading, isError, error } = useGetManagementQuery("/v1/teams?populate=*&filters[role][$eq]=management&sort=priority:ASC");
-  console.log("team", teams);
   const managements = employeeData.management.management_card;
+
+  const teamComponentArray = useMemo(() => {
+    /**
+     * i = 0
+     * j = 0
+     * i % 5 == 0 ? i = 0 : null
+     *
+     * col1 = []
+     * col2 = []
+     * col3 = []
+     *
+     * i == 0 && priority === 1 ? col2.push(jsx) : col1.push(jxs)
+     * i < 4
+     *      ? { j == 0 ? col1.push(index > 5 && j === 0 ? <emptyCard /> : jsx): col2.push(jsx); j++;}
+     *       : col3.push(jsx); i = 0; j= 0
+     */
+
+    let i = 0;
+    let j = 0;
+    let components: any[] = [];
+    if (!teams?.data) return [];
+
+    let col1: JSX.Element[] = [];
+    let col2: JSX.Element[] = [];
+    let col3: JSX.Element[] = [];
+
+    teams.data.map((team: any, index: number) => {
+      if (i % 5 === 0 && i !== 0) {
+        i = 0;
+      }
+
+      if (i === 0) {
+        col2.push(
+          <EmployeeCard
+            image={CONFIGS.CMS_URL + team.attributes.image.data.attributes.formats.thumbnail.url}
+            alt={team.attributes.name}
+            name={team.attributes.name}
+            designation={team.attributes.designation}
+            bio={team.attributes.content}
+          />
+        );
+      } else if (i > 0 && i < 4) {
+        // { j == 0 ? col1.push(index > 5 && j === 0 ? <emptyCard /> : jsx): col2.push(jsx); j++;}
+        console.log({ j, index });
+
+        if (j === 0 && index > 5) {
+          col2.push(
+            <>
+              <div></div>
+              <EmployeeCard
+                image={CONFIGS.CMS_URL + team.attributes.image.data.attributes.formats.thumbnail.url}
+                alt={team.attributes.name}
+                name={team.attributes.name}
+                designation={team.attributes.designation}
+                bio={team.attributes.content}
+              />
+            </>
+          );
+        } else if (j > 0) {
+          col2.push(
+            <EmployeeCard
+              image={CONFIGS.CMS_URL + team.attributes.image.data.attributes.formats.thumbnail.url}
+              alt={team.attributes.name}
+              name={team.attributes.name}
+              designation={team.attributes.designation}
+              bio={team.attributes.content}
+            />
+          );
+        } else {
+          col1.push(
+            <EmployeeCard
+              image={CONFIGS.CMS_URL + team.attributes.image.data.attributes.formats.thumbnail.url}
+              alt={team.attributes.name}
+              name={team.attributes.name}
+              designation={team.attributes.designation}
+              bio={team.attributes.content}
+            />
+          );
+        }
+        j++;
+      } else {
+        col3.push(
+          <EmployeeCard
+            image={CONFIGS.CMS_URL + team.attributes.image.data.attributes.formats.thumbnail.url}
+            alt={team.attributes.name}
+            name={team.attributes.name}
+            designation={team.attributes.designation}
+            bio={team.attributes.content}
+          />
+        );
+
+        let data = [
+          <>
+            <div className="col author-card" key={i}>
+              <div className="row">{col1}</div>
+            </div>
+            <div className="col author-card" key={i}>
+              <div className="row">{col2}</div>
+            </div>
+            <div className="col author-card" key={i}>
+              <div className="row">{col3}</div>
+            </div>
+          </>,
+        ];
+
+        components.push(data);
+        j = 0;
+        col1 = [];
+        col2 = [];
+        col3 = [];
+      }
+      i++;
+    });
+
+    console.log({
+      data: teams.data,
+      col1,
+      col2,
+      col3,
+      components,
+    });
+
+    return components;
+  }, [teams]);
 
   return (
     <>
@@ -18,24 +142,12 @@ const Management = () => {
         </div>
       </section>
 
-      <section id="employee" className="employee section-gap base-bg-white">
+      <section id="employee" className="employee section-gap base-bg-light">
         <div className="container">
-          <div className="row gy-5">
-            {teams &&
-              teams.data.map((team: any, i: any) => {
-                return (
-                  <div className="col-lg-4 author-card" key={i}>
-                    <EmployeeCard
-                      image={CONFIGS.CMS_URL + team.attributes.image.data.attributes.formats.thumbnail.url}
-                      alt={team.attributes.name}
-                      name={team.attributes.name}
-                      designation={team.attributes.designation}
-                      bio={team.attributes.content}
-                    />
-                  </div>
-                );
-              })}
-          </div>
+          {teamComponentArray.map((item) => (
+            <div className="row gy-5 section-parent align-items-center">{item}</div>
+          ))}
+          {/* <div className="row gy-5 section-parent align-content-around justify-content-center">{teamComponentArray}</div> */}
         </div>
       </section>
     </>
@@ -43,3 +155,60 @@ const Management = () => {
 };
 
 export default Management;
+
+// if (i === 0) {
+//   console.log("First Section", i);
+
+//   components.push(
+//     <div className="col author-card" key={i}>
+//       <div className="row">
+//         <EmployeeCard
+//           image={CONFIGS.CMS_URL + team.attributes.image.data.attributes.formats.thumbnail.url}
+//           alt={team.attributes.name}
+//           name={team.attributes.name}
+//           designation={team.attributes.designation}
+//           bio={team.attributes.content}
+//         />
+//       </div>
+//     </div>
+//   );
+// } else if (i <= 3 && i > 0) {
+//   console.log("Second Section", i);
+//   rows.push(
+//     <div className="row" key={i}>
+//       <EmployeeCard
+//         image={CONFIGS.CMS_URL + team.attributes.image.data.attributes.formats.thumbnail.url}
+//         alt={team.attributes.name}
+//         name={team.attributes.name}
+//         designation={team.attributes.designation}
+//         bio={team.attributes.content}
+//       />
+//     </div>
+//   );
+// } else {
+//   console.log("Last Section", i);
+//   components.push(
+//     <>
+//       <div className="col author-card" key={i - 1}>
+//         {rows}
+//       </div>
+//       <div className="col author-card" key={i}>
+//         <div className="row">
+//           <EmployeeCard
+//             image={CONFIGS.CMS_URL + team.attributes.image.data.attributes.formats.thumbnail.url}
+//             alt={team.attributes.name}
+//             name={team.attributes.name}
+//             designation={team.attributes.designation}
+//             bio={team.attributes.content}
+//           />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+// });
+// console.log({
+// data: teams?.data,
+// components,
+// rows,
+// });
