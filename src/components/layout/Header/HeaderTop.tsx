6c487f6e6ab16ed40a630849c24aaf5b8e9ccd83
Ifrat Binte <ifratbinte@gmail.com>
2023-01-15 10:41:08 +0600

    feat : ticker added

--- src/components/layout/Header/HeaderTop.tsx
+++ src/components/layout/Header/HeaderTop.tsx
@@ -1,5 +1,7 @@
-import PhoneContact from "#components/common/PhoneContact";
+import contactItem from "#mocks/contact";
+import tickerData from "#mocks/jsonData/ticker.json";
 import { useEffect, useState } from "react";
+import Marquee from "react-fast-marquee";
 
 function HeaderTop() {
   // sticky header hide show
@@ -17,18 +19,27 @@ function HeaderTop() {
     <>
       <section id="topBar" className="topBar text-white" style={{ display: display }}>
         <div className="row ">
-          <div className="col-md-4 topBar-left">
-            <PhoneContact />
+          <div className="col-lg-4 topBar-left">
+            <div className="section-topContact">
+              {/* <FiPhoneCall className="icon me-2" /> */}
+              <img src="/gif/phone-icon.gif" alt="" className="icon" />
+              {/* <video loop src="/gif/phone-icon.gif"></video> */}
+              <span className="fw-semibold font-Poppins"> Call Now : {contactItem[0].phone}</span>
+            </div>
           </div>
-          <div className="col-md-8 topBar-right">
-            <marquee className="ticker fw-semibold fs-18">Benemoy Securities Ltd</marquee>
-            {/* <SocialIcon /> */}
+          <div className="col-lg-8 topBar-right">
+            <Marquee className="marquee-overlay" speed={50} style={{ width: "100%" }}>
+              <ul className="list">
+                {tickerData.ticker &&
+                  tickerData.ticker.map((ticker: any, i: number) => (
+                    <li key={i}>
+                      <span className="fw-medium me-2">{ticker.title}</span> <span className="danger fw-medium">{ticker.item}</span>
+                    </li>
+                  ))}
+              </ul>
+            </Marquee>
           </div>
         </div>
-        {/* <div className="d-flex align-items-center justify-content-between">
-          
-          
-        </div> */}
       </section>
     </>
   );
