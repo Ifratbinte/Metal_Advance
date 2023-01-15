c02a556578d029efb5c1cbf454421f821b46e65b
Ifrat Binte <ifratbinte@gmail.com>
2023-01-15 10:39:02 +0600

    refactor : refactor heo section

--- src/components/pages/Home/Hero.tsx
+++ src/components/pages/Home/Hero.tsx
@@ -1,35 +1,48 @@
-import heroContent from "#mocks/jsonData/home.json";
+import heroContent from "#mocks/jsonData/hero.json";
 import { Link } from "react-router-dom";
 
 const Hero = () => {
-  // const hero = heroContent.home.hero.slider;
   const hero = heroContent.home.hero;
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // autoplay: true,
-  };
+
   return (
     <section id="hero" className="hero">
-      <div className="container ">
-        <div className="d-flex flex-column justify-content-center align-items-center text-center">
-          <h1 className="banner-title fs-45 font-Poppins fw-semibold text-light">{hero.title}</h1>
-          <p className="banner-desc font-Poppins fs-16 fw-normal">{hero.desc}</p>
-          <ul className="list-multiple list">
-            <li>
-              <Link to="/service" className="btn button-base-primary font-Poppins">
-                Invest for Future
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" className="btn button-base-warning-outline font-Poppins">
-                Get In Touch
-              </Link>
-            </li>
-          </ul>
+      <div className="container">
+        <div className="gradient-shadow-top"></div>
+        <div className="row align-items-center banner-gfx">
+          <div className="col-lg-5">
+            <div className="d-flex flex-column">
+              <h1 className="banner-title fs-45 font-Poppins fw-semibold text-light">
+                Smart <span className="fw-normal secondary">Investment</span> for <span className="fw-semibold secondary">Better</span> Future
+              </h1>
+              <p className="banner-desc font-Poppins fs-16 fw-normal">{hero.desc}</p>
+              <ul className="list-multiple list">
+                <li>
+                  <Link to="/contact" className="btn button-gradient-primary font-Poppins">
+                    Get In Touch
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-6 offset-lg-1">
+            <div className="row banner-service">
+              {hero.banner_service &&
+                hero.banner_service.map((item: any, i: number) => {
+                  return (
+                    <div className="col-md-4 col-sm-6 col">
+                      <div className="banner-service-item">
+                        <div className="item-icon">
+                          <img src={item.icon} alt={item.title} />
+                        </div>
+                        <div className="item-title">
+                          <h6 className="text-uppercase">{item.title}</h6>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
         </div>
       </div>
     </section>
